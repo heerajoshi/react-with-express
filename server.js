b49ddf2981fb222e1ad8dd8b076df90f38084ddf
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2019-04-17 12:23:38 +0530

    add biuld in server;

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8800;
 
+app.get("/hello", (req, res) => {
+  res.send("i am back ");
+});
 
-app.get('/hello',(req,res)=>{
-  res.send("i am back ")
-})
-app.listen(PORT,console.log(`listen on ${PORT}`));
+app.use(express.static("react-app/build"));
+
+app.listen(PORT, console.log(`listen on ${PORT}`));
