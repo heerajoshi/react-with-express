f121e8c39694d380939b92cc946762bd758726d0
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2019-04-19 12:19:58 +0530

    add db in server

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-
-  constructor(props){
-    super(props)
-    this.state = {text : "hello"};
+  constructor(props) {
+    super(props);
+    this.state = { text: "hello", users: [] };
     this.update = this.update.bind(this);
+    this.addUser = this.addUser.bind(this);
+  }
+  componentDidMount() {
+    fetch("/getUsers")
+      .then(res => res.json())
+      .then(res => this.setState({ users: res }));
   }
 
-  update(data){
-    this.setState({text:data});
+  update(data) {
+    this.setState({ text: data });
+  }
+
+  addUser() {
+    const username = document.getElementById("username").value;
+    fetch("/addUser", { method: "POST", body: JSON.stringify(username) })
+      .then(res => res.json())
+      .then(res => this.setState({ users: res }));
   }
 
   render() {
+    const users = this.state.users.map(user => {
+      return <div>{user.name}</div>;
+    });
     return (
       <div className="App">
-       <h1 onClick={()=>{
-         fetch("/hello").
-         then(res=>res.text()).then(this.update)
-       }}>{this.state.text}</h1>
+        <input type="text" name="username" id="username" />
+        <button onClick={this.addUser}>addUser</button>
 
+        <h1
+          onClick={() => {
+            fetch("/hello")
+              .then(res => res.text())
+              .then(this.update);
+          }}
+        >
+          {this.state.text}
+          {users}
+        </h1>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8800;
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "login_users",
+  password: process.env.DB_PASSWORD
+});
+
+const getUsers = function(req, res) {
+  connection.query(`select name from users`, (err, data, fields) =>
+    res.send(data)
+  );
+};
+
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("react-app/build"));
 
 app.get("/hello", (req, res) => {
   res.send("i am back ");
+});
+
+app.get("/getUsers", (req, res) => {
+  getUsers(req, res);
+});
+
+app.post("/addUser", (req, res) => {
+  console.log(req.body);
+  const addUserInDb = `insert into users (name) values (${req.body})`;
+  connection.query(addUserInDb, (error, results) => {
+    if (error) throw error;
+  });
+  getUsers(req, res);
 });
 
 app.listen(PORT, console.log(`listen on ${PORT}`));
