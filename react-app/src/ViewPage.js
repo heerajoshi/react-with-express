a7db714faac8b99c23bfde9cc296ee409122d2ba
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2019-08-18 13:33:34 +0530

    add some message

--- react-app/src/ViewPage.js
+++ react-app/src/ViewPage.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class AddNoPage extends Component{
-  render(){
-    return(<div><h1>Hello you are in new page by useing route</h1></div>)
+class AddNoPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello you are in new page by useing route add some change</h1>
+      </div>
+    );
   }
 }
 export default AddNoPage;
